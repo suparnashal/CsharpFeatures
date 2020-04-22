9f4d70255fd3b3c77bfc0d5274a9dc8f85a03912
suparnashal <suparnashal@gmail.com>
2020-04-22 15:37:10 -0500

    s

---
+++ ConsoleAppForCsharp8/LinQPractice/SetTwo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppForCsharp8.LinQPractice
+{
+    public class SetTwo
+    {
+
+        public static void Test_Select_Many()
+        {
+            var result = Student.GetAllStudetns().SelectMany(s => s.Subjects).OrderBy(s=>s).Distinct();
+
+            foreach(var v in result)
+            {
+                Console.WriteLine($"{v}");
+            }
+
+        }
+
+        public static void Test_Select_Many2()
+        {
+            var result = Student.GetAllStudetns().Where(s => s.Gender == "Male")
+                        .SelectMany(s => s.Subjects, (student, subject) => new { Name = student.Name, Subject = subject });
+                        
+            foreach (var v in result)
+            {
+                Console.WriteLine($"{v.Name} -  {v.Subject}");
+            }
+
+        }
+    }
+}
---
+++ ConsoleAppForCsharp8/LinQPractice/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppForCsharp8.LinQPractice
+{
+   public class Student
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public List<string> Subjects { get; set; }
+
+        public static List<Student> GetAllStudetns()
+        {
+            List<Student> listStudents = new List<Student>
+        {
+            new Student
+            {
+                Name = "Tom",
+                Gender = "Male",
+                Subjects = new List<string> { "ASP.NET", "C#" }
+            },
+            new Student
+            {
+                Name = "Mike",
+                Gender = "Male",
+                Subjects = new List<string> { "ADO.NET", "C#", "AJAX" }
+            },
+            new Student
+            {
+                Name = "Pam",
+                Gender = "Female",
+                Subjects = new List<string> { "WCF", "SQL Server", "C#" }
+            },
+            new Student
+            {
+                Name = "Mary",
+                Gender = "Female",
+                Subjects = new List<string> { "WPF", "LINQ", "ASP.NET" }
+            },
+        };
+
+            return listStudents;
+        }
+    }
+}
